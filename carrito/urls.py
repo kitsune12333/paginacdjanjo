43271c39001a39b0927e776f78eef9d0c836f3d7
incubox <be.salasm@duocuc.cl>
2023-06-18 17:16:01 -0400

    actualizando vistas y plantillas

--- carrito/urls.py
+++ carrito/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.carrito_list, name='carrito_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
--- carrito/views.py
+++ carrito/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Publicacion
+from django.shortcuts import render, get_object_or_404
 
 def carrito_list(request):
     publicacion = Publicacion.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'carritos/carrito_list.html', {'publicacion': publicacion})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Publicacion, pk=pk)
+    return render(request, 'carritos/post_detail.html', {'post': post})
