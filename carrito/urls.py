3a1c894b72a30e178c46110eb950b0b8bb7e4dcf
incubox <be.salasm@duocuc.cl>
2023-06-18 23:07:35 -0400

    vistas para crear y editar post

--- carrito/urls.py
+++ carrito/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('carrito_list/', views.carrito_list, name='carrito_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new', views.post_new, name='post_new'),
 ]
--- carrito/views.py
+++ carrito/views.py
@@ -1,6 +1,7 @@
 from django.utils import timezone
 from .models import Publicacion
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from .forms import PostForm
 
 def index(request):
     return render(request, 'carritos/index.html')
@@ -12,4 +13,31 @@ def carrito_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Publicacion, pk=pk)
     return render(request, 'carritos/post_detail.html', {'post': post})
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
 
