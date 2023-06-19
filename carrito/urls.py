8ccd843705bea260151f7705f18a97da333cd905
incubox <be.salasm@duocuc.cl>
2023-06-18 22:36:59 -0400

    imagenes y index

--- carrito/urls.py
+++ carrito/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.carrito_list, name='carrito_list'),
+    path('', views.index, name='index'),
+    path('carrito_list/', views.carrito_list, name='carrito_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
--- carrito/views.py
+++ carrito/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
 from django.utils import timezone
 from .models import Publicacion
 from django.shortcuts import render, get_object_or_404
+
+def index(request):
+    return render(request, 'carritos/index.html')
 
 def carrito_list(request):
     publicacion = Publicacion.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -10,3 +12,4 @@ def carrito_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Publicacion, pk=pk)
     return render(request, 'carritos/post_detail.html', {'post': post})
+
